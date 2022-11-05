51a1485ba1605b1bb6f6bded16eb095713d42f5a
Juan Pablo Alba Fernandez <juanpialba@gmail.com>
2022-11-05 18:48:18 -0300

    se generan card

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./components/Footer/Footer";
+
+const App = () => {
+  return (
+    <>
+    <h1 id="titulo">Hello Word</h1>
+    <p id="descripcion">Esta es mi primera pagina de React!</p>
+    <Footer/>    
+    </>
+  );
+}
+
+export default App;
